ffa2ab97b0870c94e46b4d782cd0c94efaaa5fc7
Zelriong <simo00cane@gmail.com>
2026-02-20 12:02:47 +0100

    Random

--- Assets/Scripts/Blocchi/LevelManager.cs
+++ Assets/Scripts/Blocchi/LevelManager.cs
@@ -48,6 +48,8 @@ public class LevelManager : MonoBehaviour               //Willy
         {
             currentBlock = totalBlock;
 
+
+            
             Destroy(currentLevel);
 
             random = Random.Range(0, Level.Count);
